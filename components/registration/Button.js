75201ed78fb57e35c488c64e0a8210367367a032
Andryukhina Alena <alena.andryukhina@dunice.net>
2018-04-28 16:01:56 +0300

    Add icons for navigation buttons

--- components/registration/Button.js
+++ components/registration/Button.js
@@ -1,7 +1,10 @@
+import MdArrowBack from 'react-icons/lib/md/arrow-back';
+import MdArrowForward from 'react-icons/lib/md/arrow-forward';
+
 const Button = () => (
   <div className='buttons__container'>
-    <button type='button' className='btn--navigate btn--prev'>&larr;</button>
-    <button type='button' className='btn--navigate btn--next'>&rarr;</button>
+    <button type='button' className='btn--navigate btn--prev'> <MdArrowBack /> </button>
+    <button type='button' className='btn--navigate btn--next'> <MdArrowForward /> </button>
   </div>
 );
 
--- components/registration/Header.js
+++ components/registration/Header.js
@@ -1,7 +1,9 @@
+import MdArrowBack from 'react-icons/lib/md/arrow-back';
+
 const Header = () => (
     <div className='header'>
       <div className='row no-gutters header__block'>
-        <a href='#' className="header__block__left-link">&larr;Back</a>
+        <a href='#' className="header__block__left-link"> <MdArrowBack />Back</a>
         <a href='#'>Logo</a>
         <a href='#' className="header__block__right-link">I have an account (Login)</a>
       </div>
