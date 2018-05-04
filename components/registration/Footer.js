74a3660e06edcc9059cab54a6b5521cc3377b8f0
Andryukhina Alena <alena.andryukhina@dunice.net>
2018-05-04 11:01:58 +0300

    Change footer for registration pages

--- components/registration/Footer.js
+++ components/registration/Footer.js
@@ -1,7 +1,18 @@
 const RegistartionFooter = () => (
   <div className='footer'>
     <div className='footer__text'>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      <p>To help the government fight the funding of terrorism and money laundering activities, Federal law requires all financial institutions to obtain, verify, and record information that identifies each person who opens an account. To meet these requirements, when you open an
+        account, we will ask for your name, address, date of birth, and other information that will allow us to identify you. We may also utilize a third-party information provider for verification purposes and/or ask for a copy of your driver’s license or other identifying documents.
+        Please see the Customer Identification Program (CIP) Notice for further information.</p>
+      <p>All investments involve risk and the past performance of a security, or financial product does not guarantee future results or returns.  Keep in mind that while diversification may help spread risk it does not assure a profit, or protect against loss, in a down market. There is
+        always the potential of losing money when you invest in securities, or other financial products.  Investors should consider their investment objectives and risks carefully before investing.</p>
+      <p>All securities and investments are offered to self-directed customers by Empala Securities, LLC, a member of FINRA & the SIPC.  Empala Securities, LLC is a wholly owned subsidiary of Empala Group, LLC.   Additional information about Empala Securities, LLC and
+        Empala Group, LLC can be found by visiting the FINRA BrokerCheck site.</p>
+      <span className='float-left margin-right'>Empala Securities Terms and Conditions</span>
+      <span className='margin-right'>Disclosure Library</span>
+      <span className='margin-right'>Contact Us</span>
+      <span className='margin-right'>FAQ</span>
+      <span className='float-right'>© 2018 Empala. All rights reservered.</span>
     </div>
   </div>
 );
