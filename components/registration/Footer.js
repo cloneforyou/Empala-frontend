64607f8ca7cc50b0a7c5cdfbe5f75ab6bcb31afb
Andryukhina Alena <alena.andryukhina@dunice.net>
2018-05-08 12:52:28 +0300

    change the style of footer

--- components/registration/Footer.js
+++ components/registration/Footer.js
@@ -1,6 +1,6 @@
 const RegistartionFooter = () => (
   <div className='footer'>
-    <div className='footer__text'>
+    <div className='footer--text'>
       <p>To help the government fight the funding of terrorism and money laundering activities, Federal law requires all financial institutions to obtain, verify, and record information that identifies each person who opens an account. To meet these requirements, when you open an
         account, we will ask for your name, address, date of birth, and other information that will allow us to identify you. We may also utilize a third-party information provider for verification purposes and/or ask for a copy of your driver’s license or other identifying documents.
         Please see the Customer Identification Program (CIP) Notice for further information.</p>
@@ -8,11 +8,17 @@ const RegistartionFooter = () => (
         always the potential of losing money when you invest in securities, or other financial products.  Investors should consider their investment objectives and risks carefully before investing.</p>
       <p>All securities and investments are offered to self-directed customers by Empala Securities, LLC, a member of FINRA & the SIPC.  Empala Securities, LLC is a wholly owned subsidiary of Empala Group, LLC.   Additional information about Empala Securities, LLC and
         Empala Group, LLC can be found by visiting the FINRA BrokerCheck site.</p>
-      <span className='float-left margin-right'>Empala Securities Terms and Conditions</span>
-      <span className='margin-right'>Disclosure Library</span>
-      <span className='margin-right'>Contact Us</span>
-      <span className='margin-right'>FAQ</span>
-      <span className='float-right'>© 2018 Empala. All rights reservered.</span>
+      <div className='footer--disclaimer'>
+        <div>
+          <a href="#" className='footer--link'>Empala Securities Terms and Conditions</a>
+          <a href="#" className='footer--link'>Disclosure Library</a>
+          <a href="#" className='footer--link'>Contact Us</a>
+          <a href="#" className='footer--link'>FAQ</a>
+        </div>
+        <div>
+          <span>© 2018 Empala. All rights reservered.</span>
+        </div>
+      </div>
     </div>
   </div>
 );
