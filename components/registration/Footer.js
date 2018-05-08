d3810efb17e6b784b653e6a0fc459d055112acd1
Andryukhina Alena <alena.andryukhina@dunice.net>
2018-05-08 13:53:30 +0300

    add media query for the footer

--- components/registration/Footer.js
+++ components/registration/Footer.js
@@ -1,7 +1,7 @@
 const RegistartionFooter = () => (
   <div className='footer'>
     <div className='footer--text'>
-      <p>To help the government fight the funding of terrorism and money laundering activities, Federal law requires all financial institutions to obtain, verify, and record information that identifies each person who opens an account. To meet these requirements, when you open an
+      <p className='text--info'>To help the government fight the funding of terrorism and money laundering activities, Federal law requires all financial institutions to obtain, verify, and record information that identifies each person who opens an account. To meet these requirements, when you open an
         account, we will ask for your name, address, date of birth, and other information that will allow us to identify you. We may also utilize a third-party information provider for verification purposes and/or ask for a copy of your driver’s license or other identifying documents.
         Please see the Customer Identification Program (CIP) Notice for further information.</p>
       <p>All investments involve risk and the past performance of a security, or financial product does not guarantee future results or returns.  Keep in mind that while diversification may help spread risk it does not assure a profit, or protect against loss, in a down market. There is
