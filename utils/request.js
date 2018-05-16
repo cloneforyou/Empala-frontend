c775dfdaa052a3254f73d65d342d3ee4da00dafd
Yaroslav Kulikov <y.kulikov@dunice.net>
2018-05-16 17:15:36 +0300

    changed urls in sagas, saves tokens in localStorage when registered successfuly

--- utils/request.js
+++ utils/request.js
@@ -12,7 +12,8 @@ export default function request(url, options = {}) {
     withCredentials: true
   })
     .then(response => {
-      console.log(' *** res', response);
+      // console.log(' ** request response', response);
+      return response;
     })
     .catch(err => {
       // console.log(' *** ------------> SERVER RESPOND ERROR', JSON.stringify(err));
@@ -29,55 +30,4 @@ export default function request(url, options = {}) {
       }
     }
   }
-
-  // const endpoint = '';
-  // const defaultHeaders = {
-  //   'X-Requested-With': 'XMLHttpRequest',
-  //   Accept: 'application/json',
-  //   // Accept: '*/*',
-  //   'Content-Type': 'application/json',
-  // };
-  //
-  // const requestOptions = {
-  //   ...options,
-  //   headers: Object.assign(defaultHeaders, options.headers)
-  // };
-  //
-  // // Missing boundary in multipart/form-data fix https://stackoverflow.com/a/41604180/2156864
-  // if (requestOptions.headers['Content-Type'] === 'multipart/form-data') {
-  //   delete requestOptions.headers['Content-Type'];
-  // }
-  //
-  // requestOptions.withCredentials =  true;
-  // requestOptions.credentials = 'include';
-  // requestOptions.mode = 'cors';
-  //
-  // let requestUrl = url;
-  // // add default endpoint only to relative urls
-  // if (requestUrl.indexOf('://') === -1) {
-  //   requestUrl = endpoint + requestUrl;
-  // }
-  //
-  // return new Promise((resolve, reject) => {
-  //   console.log(' *** ', requestUrl, requestOptions);
-  //   fetch(`http://localhost:9000/auth/check`, requestOptions)
-  //     .then(response => {
-  //       if (response.status >= 200 && response.status < 400) {
-  //         if (response.redirected === true) {
-  //           return response;
-  //         }
-  //         if(response.headers.get('Content-Type') === 'application/json'){
-  //           return response.json()
-  //         }
-  //         else{
-  //           return response.text();
-  //         }
-  //       }
-  //       return reject(response);
-  //     })
-  //     .then((response) => {
-  //       return resolve(response);
-  //     })
-  //     .catch((error) => reject({ message: error }));
-  // });
 }
