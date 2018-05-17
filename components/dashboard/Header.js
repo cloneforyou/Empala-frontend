82dc6a2bf8943c774ac5f07c8225f656783499e6
Irenese <i.martyshko@dunice.net>
2018-05-17 14:38:03 +0300

    styled footer and added mini-logo for hiden sidebar

--- components/dashboard/Header.js
+++ components/dashboard/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import logo from '../../static/images/logo.png';
+import iconLogo from '../../static/images/dashboard-icons/icon-logo.svg';
 
 class Header extends Component {
   constructor(props) {
@@ -8,21 +9,25 @@ class Header extends Component {
   }
 
   render() {
-    const { sidebarCollapsed } = this.props
+    const {sidebarCollapsed} = this.props
     return (
       <div
         className={sidebarCollapsed ? "navbar fixed-top flex-md-nowrap p-0 dashboard-header dashboard-header_collapsed" : "navbar fixed-top flex-md-nowrap p-0 dashboard-header"}>
         <a className="navbar-brand mr-0 dashboard-header__logo" href="#">
-          <img src={logo} alt=""/>
+          {
+            sidebarCollapsed ?
+              <img src={logo} alt="" /> :
+              <img src={iconLogo} alt="" />
+          }
         </a>
         <div className='dashboard-header__main row'>
           <button
             className="navbar-toggler collapsed dashboard-header__toogler"
             onClick={this.props.collapseMenu}
           >
-            <span className="navbar-toggler-icon"/>
+            <span className="navbar-toggler-icon" />
           </button>
-          <input className="form-control col-6" type="text" placeholder="Search" aria-label="Search"/>
+          <input className="form-control col-6" type="text" placeholder="Search" aria-label="Search" />
           <ul className="navbar-nav px-3 col-4">
             <li className="nav-item text-nowrap">
               <a className="nav-link" href="#">Sign out</a>
