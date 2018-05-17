669b64456257cded3f11e066baebaea5873a8499
Irenese <i.martyshko@dunice.net>
2018-05-17 16:05:26 +0300

    layout header

--- components/dashboard/Header.js
+++ components/dashboard/Header.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 import logo from '../../static/images/logo.png';
 import iconLogo from '../../static/images/dashboard-icons/icon-logo.svg';
+import avatar from '../../static/images/avatar-user.svg';
 
 class Header extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   render() {
-    const {sidebarCollapsed} = this.props
+    const {sidebarCollapsed} = this.props;
     return (
       <div
-        className={sidebarCollapsed ? "navbar fixed-top flex-md-nowrap p-0 dashboard-header dashboard-header_collapsed" : "navbar fixed-top flex-md-nowrap p-0 dashboard-header"}>
+        className={sidebarCollapsed ?
+          'navbar fixed-top flex-md-nowrap p-0 dashboard-header dashboard-header_collapsed' :
+          'navbar fixed-top flex-md-nowrap p-0 dashboard-header'
+        }
+      >
         <a className="navbar-brand mr-0 dashboard-header__logo" href="#">
           {
             sidebarCollapsed ?
@@ -20,31 +25,48 @@ class Header extends Component {
               <img src={iconLogo} alt="" />
           }
         </a>
-        <div className='dashboard-header__main row'>
+        <div className="dashboard-header__main row justify-content-between">
           <button
             className="navbar-toggler collapsed dashboard-header__toogler"
             onClick={this.props.collapseMenu}
           >
             <span className="navbar-toggler-icon" />
           </button>
-          <input className="form-control col-6" type="text" placeholder="Search" aria-label="Search" />
-          <ul className="navbar-nav px-3 col-4">
-            <li className="nav-item text-nowrap">
-              <a className="nav-link" href="#">Sign out</a>
+          <input className="form-control col" type="text" placeholder="Search" aria-label="Search" />
+          <ul className="nav user-nav align-items-center">
+            <li className="nav-item">
+              <a className="nav-link user-nav__link" href="#">
+                <i className="user-nav__icon user-nav__icon_chat" />
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link user-nav__link" href="#">
+                <i className="user-nav__icon user-nav__icon_notification" />
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle">
+                <img src={avatar} alt="" />
+              </a>
+              <div className="dropdown-menu">
+                <a className="dropdown-item" href="#">Action</a>
+                <a className="dropdown-item" href="#">Another action</a>
+                <a className="dropdown-item" href="#">Something else here</a>
+              </div>
             </li>
           </ul>
         </div>
       </div>
-    )
+    );
   }
 }
 
 function mapStateToProps(state) {
-  return {}
+  return {};
 }
 
 function mapDispatchToProps(dispatch) {
-  return {}
+  return {};
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Header)
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
