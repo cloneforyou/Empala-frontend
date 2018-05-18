652decb62d9366c767fe8cc18b96a354f010c050
Irenese <i.martyshko@dunice.net>
2018-05-18 09:05:38 +0300

    reorginize header component

---
+++ components/dashboard/Header/Search.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="col dashboard-search">
+        <button className="dashboard-search__btn">
+          <i className="dashboard-search__icon" />
+        </button>
+        <input
+          className="form-control dashboard-search__input"
+          type="text"
+          placeholder="Search"
+        />
+      </div>
+    )
+  }
+}
+export default Search
