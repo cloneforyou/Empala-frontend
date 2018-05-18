1a4e266bca844130527f8afb56a15588c95a055b
Andryukhina Alena <alena.andryukhina@dunice.net>
2018-05-18 11:35:13 +0300

    change color for calendars and active fields; refactor code

---
+++ constants/colors.js
@@ -0,0 +1,8 @@
+export const GREEN = '#98C73A';
+export const MAIN_COLOR_TEXT_GRAY = '#808895';
+export const LIGHT_GREY = '#C5C5C5';
+export const IRON = '#D0D2D4';
+export const ALTO = '#E0E0E0';
+
+
+
