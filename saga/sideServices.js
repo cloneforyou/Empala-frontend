f823cf43046cd16e2b0b61f11012157c4059e432
Yaroslav Kulikov <y.kulikov@dunice.net>
2018-05-22 10:48:08 +0300

    profile:employement state displays as short label

--- saga/sideServices.js
+++ saga/sideServices.js
@@ -1,121 +1,51 @@
-import {put} from 'redux-saga/effects';
-import {js2xml, xml2js} from "xml-js";
-import {setInputFieldValueById} from "../actions/registration";
+import { put } from 'redux-saga/effects';
+import { js2xml, xml2js } from 'xml-js';
+import { setInputFieldValueById } from '../actions/registration';
+import { statesAbbvs } from '../localdata/usStatesList';
 
-const statesAbbvs = {
-  AL: 'Alabama',
-  AK: 'Alaska',
-  AS: 'American Samoa',
-  AZ: 'Arizona',
-  AR: 'Arkansas',
-  CA: 'California',
-  CO: 'Colorado',
-  CT: 'Connecticut',
-  DE: 'Delaware',
-  DC: 'District of Columbia',
-  FM: 'Federated Stated of Micronesia',
-  FL: 'Florida',
-  GA: 'Georgia',
-  GU: 'Guam',
-  HI: 'Hawaii',
-  ID: 'Idaho',
-  IL: 'Illinois',
-  IN: 'Indiana',
-  IA: 'Iowa',
-  KS: 'Kansas',
-  KY: 'Kentucky',
-  LA: 'Louisiana',
-  ME: 'Maine',
-  MH: 'Marshall Islands',
-  MD: 'Maryland',
-  MA: 'Massachusetts',
-  MI: 'Michigan',
-  MN: 'Minnesota',
-  MS: 'Mississippi',
-  MO: 'Missouri',
-  MT: 'Montana',
-  NE: 'Nebraska',
-  NV: 'Nevada',
-  NH: 'New Hampshire',
-  NJ: 'New Jersey',
-  NM: 'New Mexico',
-  NY: 'New York',
-  NC: 'North Carolina',
-  ND: 'North Dakota',
-  MP: 'Northern Mariana Islands',
-  OH: 'Ohio',
-  OK: 'Oklahoma',
-  OR: 'Oregon',
-  PW: 'Palau',
-  PA: 'Pennsylvania',
-  PR: 'Puerto Rico',
-  RI: 'Rhode Island',
-  SC: 'South Carolina',
-  SD: 'South Dakota',
-  TN: 'Tennessee',
-  TX: 'Texas',
-  UT: 'Utah',
-  VT: 'Vermont',
-  VI: 'Virgin Islands',
-  VA: 'Virginia',
-  WA: 'Washington',
-  WV: 'West Virginia',
-  WI: 'Wisconsin',
-  WY: 'Wyoming',
-  // AA: 'Armed Forces Americas',
-  // AE: 'Armed Forces Africa',
-  // AE: 'Armed Forces Canada',
-  // AE: 'Armed Forces Europe',
-  // AE: 'Armed Forces Middle East',
-  // AP: 'Armed Forces Pacific',
-};
-
-export function* getAddressInfoByZIP({fieldId, zipCode}) {
-  console.log(' ** ZZZZIIIIPP' );
+export function* getAddressInfoByZIP({ fieldId, zipCode }) {
   const urlBase = 'http://production.shippingapis.com/ShippingAPI.dll?API=CityStateLookup&XML=';
   const clientId = '018EMPAL1274';
   const data = {
-    "elements": [
+    elements: [
       {
-        "type": "element",
-        "name": "CityStateLookupRequest",
-        "attributes": {USERID: clientId},
-        "elements": [
+        type: 'element',
+        name: 'CityStateLookupRequest',
+        attributes: { USERID: clientId },
+        elements: [
           {
-            "type": "element",
-            "name": "ZipCode",
-            'attributes': {ID: 0},
-            "elements": [
+            type: 'element',
+            name: 'ZipCode',
+            attributes: { ID: 0 },
+            elements: [
               {
-                "type": "element",
-                "name": "Zip5",
-                "elements": [
+                type: 'element',
+                name: 'Zip5',
+                elements: [
                   {
-                    "type": "text",
-                    "text": zipCode
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    ]
+                    type: 'text',
+                    text: zipCode,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
   };
   const url = `${urlBase}${js2xml(data)}`;
   try {
     const info = {};
-    yield fetch(url).then(
-        (res) => res.text()
-            .then((text) => {
-              const result = xml2js(text, {compact: true}).CityStateLookupResponse.ZipCode;
-              // console.log('**** CITY *****', result);
-              if (!result.Error) {
-                    info.city = result.City._text;
-                    info.state = statesAbbvs[result.State._text];
-              } info.error = result.Error
-            }
-            ));
+    yield fetch(url).then(res => res.text()
+      .then((text) => {
+        const result = xml2js(text, { compact: true }).CityStateLookupResponse.ZipCode;
+        // console.log('**** CITY *****', result);
+        if (!result.Error) {
+          info.city = result.City._text;
+          info.state = statesAbbvs[result.State._text];
+        } info.error = result.Error;
+      }));
     if (!info.error) {
       if (fieldId === 'identity_zip_code') {
         yield put(setInputFieldValueById('identity_residential_address_residential_address_state', info.state));
@@ -128,9 +58,7 @@ export function* getAddressInfoByZIP({fieldId, zipCode}) {
         yield put(setInputFieldValueById('profile_employment_city', info.city));
       }
     }
+  } catch (err) {
+    console.log(err);
   }
-  catch (err) {
-    console.log(err)
-  }
-
 }
