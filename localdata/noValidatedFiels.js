7e71dcffc81dbe24c500e01af7e086eeb33f1157
Andryukhina Alena <alena.andryukhina@dunice.net>
2018-06-01 16:53:44 +0300

    fix the validation for the Residential address; the Suffix selection field is optional

---
+++ localdata/noValidatedFiels.js
@@ -0,0 +1,7 @@
+const ignoredFields = [
+  'member_basic_information_suffix',
+  'identity_mailing_address_residential_address_line_2',
+  'identity_residential_address_residential_address_line_2'
+];
+
+export default ignoredFields;
