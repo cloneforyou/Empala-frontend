11b7dc076736642ee20c5a95198901cd0f9c91dc
dts-irene <irina@devteamspace.com>
2018-06-05 13:01:24 +0300

    added dropdown menu

--- components/dashboard/Header/RightBlock.js
+++ components/dashboard/Header/RightBlock.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import DropdownMenu from './DropdownMenu';
 import avatar from '../../../static/images/avatar-user.svg';
 
 class RightBlock extends Component {
@@ -18,7 +19,6 @@ class RightBlock extends Component {
   }
 
   render() {
-
     const { menuAvatarShow } = this.state;
     return (
       <div>
@@ -41,14 +41,7 @@ class RightBlock extends Component {
             >
               <img src={avatar} alt="" />
             </button>
-            <div
-              className={(menuAvatarShow === false) ?
-                'dropdown-menu dropdown-menu-right user-nav__dropdown-menu' :
-                'dropdown-menu dropdown-menu-right show user-nav__dropdown-menu'}
-            >
-              <a className="dropdown-item" href="#">Profile</a>
-              <a className="dropdown-item" href="#">Logout</a>
-            </div>
+            <DropdownMenu menuAvatarShow={menuAvatarShow}/>
           </li>
         </ul>
       </div>
