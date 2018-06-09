8a4e7029beae143cc44d63351faee698f6253d84
Yaroslav K <yaroslav.k@devteamspace.com>
2018-06-09 17:23:12 +0300

    feature: displays security warning page if account suspended; logic for unblocking account

--- saga.js
+++ saga.js
@@ -1,13 +1,14 @@
 /* global fetch */
 
 import { delay } from 'redux-saga';
-import { all, call, put, take, takeLatest } from 'redux-saga/effects';
+import { all, takeLatest } from 'redux-saga/effects';
 import es6promise from 'es6-promise';
 import 'isomorphic-unfetch';
 
 import registrationSaga from './saga/registration';
 import dashboardSaga from './saga/dashboard';
-import { authenticate } from './saga/authentication';
+import {authenticate, unblockAccount} from './saga/authentication';
+import {LOGIN_REQUEST, UNBLOCK_REQUEST} from "./constants/auth";
 
 
 es6promise.polyfill();
@@ -17,7 +18,8 @@ function* rootSaga() {
   yield all([
     registrationSaga(),
     dashboardSaga(),
-    takeLatest('LOGIN_REQUEST', authenticate),
+    takeLatest(LOGIN_REQUEST, authenticate),
+    takeLatest(UNBLOCK_REQUEST, unblockAccount),
   ]);
 }
 
