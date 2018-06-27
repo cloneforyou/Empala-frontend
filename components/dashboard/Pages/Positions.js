ef29589ae895fe34698b1f88e27d0801c5d5d1b2
dts-irene <irina@devteamspace.com>
2018-06-27 17:33:49 +0300

    added test chart

--- components/dashboard/Pages/Positions.js
+++ components/dashboard/Pages/Positions.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { widgetsPositionFirst, widgetsPositions } from '../../../localdata/dashboardWidgets';
 import WidgetTable from '../Widget/WidgetTable';
+import AnyChart from 'anychart-react';
 
 class Positions extends Component {
   constructor(props) {
@@ -24,9 +25,31 @@ class Positions extends Component {
                 <WidgetTable widget={widget} key={widget.id} />
               ))
             }
+            <div className="widget-col col-lg-12">
+              <div className="widget">
+                <div className="widget__head">
+                  <div className="widget__title-row">
+                    <h3 className="widget__title">Stacked area chart</h3>
+                  </div>
+                </div>
+                <div className="widget__body">
+                  <AnyChart
+                    type="line"
+                    data={
+                      [
+                        { x: 'Rouge', value: 80540 },
+                        { x: 'Foundation', value: 94190 },
+                        { x: 'Mascara', value: 102610 },
+                        { x: 'Lip gloss', value: 110430 },
+                        { x: 'Pomade', value: 128000 }
+                      ]
+                    }
+                    title="Simple pie chart"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
-
-
         </div>
       </div>
 
