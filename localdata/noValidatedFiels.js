cada0444b5f8a174dcf73dec2e7648030d7c64c1
Yaroslav K <yaroslav.k@devteamspace.com>
2018-09-21 16:05:26 +0300

    Registration: fixed add_margin isRequired check

--- localdata/noValidatedFiels.js
+++ localdata/noValidatedFiels.js
@@ -1,7 +1,8 @@
 const ignoredFields = [
   'member_basic_information_suffix',
   'identity_mailing_address_residential_address_line_2',
-  'identity_residential_address_residential_address_line_2'
+  'identity_residential_address_residential_address_line_2',
+  'member_account_add_margin',
 ];
 
 export default ignoredFields;
