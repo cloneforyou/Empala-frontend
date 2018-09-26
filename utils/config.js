09d5ae3f186f1b4241feb26e6e38fd86655de6ec
Andryukhina Alena <helen@devteamspace.com>
2018-09-26 14:18:17 +0300

    server origin change

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 export const serverOrigins = {
-  aws: 'http://ec2-18-217-9-54.us-east-2.compute.amazonaws.com:9000',
+  aws: 'https://dev-api.empala.com',
   local: 'http://localhost:9000',
   localSecure: 'https://localhost:55553',
 };
