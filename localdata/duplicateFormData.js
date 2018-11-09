07ef4b770e13022acd2cc3a69d10fd5ab20576a4
Yaroslav K <yaroslav.k@devteamspace.com>
2018-11-09 17:44:34 +0300

    FIX: temporary fix for build

--- localdata/duplicateFormData.js
+++ localdata/duplicateFormData.js
@@ -1,5 +1,74 @@
-import { getValuesForSelectField } from '../utils/registrationUtils';
-import { usStatesList } from './usStatesList';
+// import { getValuesForSelectField } from '../utils/registrationUtils';
+// import { usStatesList } from './usStatesList';
+
+function getValuesForSelectField(list) {
+  return list.map(el => ({
+    value: el,
+    title: el,
+  }));
+}
+
+const usStatesList = [
+  'Alabama',
+  'Alaska',
+  'American Samoa',
+  'Arizona',
+  'Arkansas',
+  'California',
+  'Colorado',
+  'Connecticut',
+  'Delaware',
+  'District of Columbia',
+  'Federated Stated of Micronesia',
+  'Florida',
+  'Georgia',
+  'Guam',
+  'Hawaii',
+  'Idaho',
+  'Illinois',
+  'Indiana',
+  'Iowa',
+  'Kansas',
+  'Kentucky',
+  'Louisiana',
+  'Maine',
+  'Marshall Islands',
+  'Maryland',
+  'Massachusetts',
+  'Michigan',
+  'Minnesota',
+  'Mississippi',
+  'Missouri',
+  'Montana',
+  'Nebraska',
+  'Nevada',
+  'New Hampshire',
+  'New Jersey',
+  'New Mexico',
+  'New York',
+  'North Carolina',
+  'North Dakota',
+  'Northern Mariana Islands',
+  'Ohio',
+  'Oklahoma',
+  'Oregon',
+  'Palau',
+  'Pennsylvania',
+  'Puerto Rico',
+  'Rhode Island',
+  'South Carolina',
+  'South Dakota',
+  'Tennessee',
+  'Texas',
+  'Utah',
+  'Vermont',
+  'Virgin Islands',
+  'Virginia',
+  'Washington',
+  'West Virginia',
+  'Wisconsin',
+  'Wyoming',
+];
 
 const usStates = getValuesForSelectField(usStatesList);
 
