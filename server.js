9989f8db92f3e4418718bf30379953c3e8118a91
Yaroslav K <yaroslav.k@devteamspace.com>
2018-12-18 10:34:16 +0300

    UPD/server.js: favicon serve

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 const express = require('express');
 const next = require('next');
-const routes = require('./routes')
+const routes = require('./routes');
 
-const https = require('https');
-const fs = require('fs');
+var favicon = require('serve-favicon');
+var path = require('path');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-const handle = app.getRequestHandler();
+// const handle = app.getRequestHandler();
 const handler = routes.getRequestHandler(app);
 
 app.prepare()
   .then(() => {
     const server = express();
-    const faviconOptions = {
-      root: __dirname + '/static/images/',
-    };
-    server.get('/favicon.ico', (req, res) => (
-      res.status(200).sendFile('icon-empala.ico', faviconOptions)
-    ));
+    server.use(favicon(path.join(__dirname, '/static/images/', 'icon-empala.svg')));
     server.use(handler);
     // server.get('*', (req, res) => {
     //   return handle(req, res)
@@ -31,16 +26,6 @@ app.prepare()
         ? 'http://ec2-18-217-9-54.us-east-2.compute.amazonaws.com/'
         : 'localhost:3000'}`);
     });
-    // https.createServer({
-    //   key: fs.readFileSync('key.pem'),
-    //   cert: fs.readFileSync('cert.pem'),
-    //   passphrase: 'empala',
-    // }, server).listen(55555, (err) => {
-    //   if (err) throw err;
-    //   console.log(`> EMPALA: ready on ${process.env.NODE_ENV === 'production'
-    //     ? 'https://ec2-18-217-9-54.us-east-2.compute.amazonaws.com/'
-    //     : 'https://localhost:55555'}`);
-    // });
   })
   .catch((ex) => {
     console.error(ex.stack);
